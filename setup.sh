c2319b1603ef30677d6f97999e2a0cba40e4a993
root <root@cassadra-poc-node-3.novalocal>
2018-11-21 10:11:46 +0700

    revert

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cp -r .vim ~
+if [ ! $(cmp --silent .vimrc ~/.vimrc) ];then
+    cp .vimrc ~
+fi
+home_dir=$(echo ~)
+vimrc="$home_dir/.vimrc"
+cd ~/.vim/bundle
+if [ -z "$(ls -A ~/.vim/bundle/pydiction)" ]; then
+    git clone https://github.com/rkulla/pydiction.git 
+fi
+if [ -f $vimrc ];then 
+    sed -i 's~/home/quy~'$home_dir'~g' $vimrc
+fi
+if [ ! -d '~/.vim/bundle/nerdcommenter' ];then 
+    git clone https://github.com/scrooloose/nerdcommenter.git
+fi
+if [ -d '~/.vim/bundle/vim' ];then
+    git clone https://github.com/dracula/vim.git
+fi
+
+
